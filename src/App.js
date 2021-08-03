33de96448c974b3f281e38ef3a48d8d779667ddd
dev A <mafaq.iplex@gmail.com>
2021-08-03 16:48:27 -0400

    Header component created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import React from 'react';
 import './App.css';
-import Introduction from './components/introduction';
+// import Introduction from './components/introduction';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-     <Introduction />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      applicationName: 'Traffic App'
+    }
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <Header appName={this.state.applicationName}  />
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import { Component } from "react";
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+
+class Header extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            isOpen: false
+        }
+
+        this.toggleNavbar = this.toggleNavbar.bind(this);
+    }
+
+    toggleNavbar(){
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
+
+    render (){
+        return (
+            <Navbar color="light" light>
+                <NavbarBrand href="/" className="mr-auto">{this.props.appName}</NavbarBrand>
+                <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+                <Collapse isOpen={this.state.isOpen} navbar>
+                    <Nav navbar>
+                        <NavItem>
+                            <NavLink href="/">1000 Traffic Accidents</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        );
+    }
+    
+}
+
+export default Header;
