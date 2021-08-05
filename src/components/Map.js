df7f9617c927c0688e85c99750c8bc56bb7c9ccb
dev A <mafaq.iplex@gmail.com>
2021-08-05 11:52:01 -0400

    refactor work has been done in map.js

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,58 +1,95 @@
-import { Component } from "react";
-import ReactMapGL, {Marker} from "react-map-gl";
-import Pin  from "./pin";
+import React from "react";
+import mapboxgl from "mapbox-gl";
 
-class Map extends Component {
+class Map extends React.Component {
     constructor(props){
         super(props);
-        
         this.state = {
             api_url: "https://data.edmonton.ca/resource/k4tx-5k8p.json",
-            viewport: {
-                width: 1100,
-                height: 600,
+            map: false,
+            viewport: { 
                 zoom: 10,
-                latitude: 53.5444,
-                longitude: -113.4909
+                center: [-113.4909, 53.5444]
             },
-            coords: [
-                { latitude: 53.5444, longitude: -113.4909 },
-                { latitude: 53.5451, longitude: -113.4923 },
-                { latitude: 53.5438, longitude: -113.5678 }
-            ],
-            token: 'pk.eyJ1IjoiYWZhcTEyMzQ1IiwiYSI6ImNrcndrY2ltbTBoZWoycG94Y3ljNW9yNXkifQ.YeekAmHni0RhHU3LAz3-1Q',
             data: null
         }
     }
 
+    initializeMap(){
+        mapboxgl.accessToken = 'pk.eyJ1IjoiYWZhcTEyMzQ1IiwiYSI6ImNrcndrY2ltbTBoZWoycG94Y3ljNW9yNXkifQ.YeekAmHni0RhHU3LAz3-1Q';
+        let map = new mapboxgl.Map({
+            container: 'map', // container ID
+            style: 'mapbox://styles/mapbox/streets-v11', // style URL
+            ...this.state.viewport
+        });
+
+        map.on('load', () => {
+            map.addLayer({
+                'id': 'points',
+                'type': 'circle',
+                'source': {
+                    "type": "geojson",
+                    "data": this.state.data
+                },
+                'paint': {
+                    'circle-radius': 6,
+                    'circle-color': '#B42222'
+                }
+            });
+            console.log("load-data", this.state.data);
+        });
+
+        this.setState({ map });
+    }
+
+    createFeatureCollection(data){
+        let feature = [];
+        data.forEach( (mark) => 
+            feature.push(
+                {
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [
+                            parseFloat(mark.point.coordinates[0]),
+                            parseFloat(mark.point.coordinates[1])   
+                        ] 
+                    },
+                    "properties": {
+                        "description": mark.description, 
+                        "duration": mark.duration,
+                        "impact": mark.impact,
+                        "details": mark.details
+                    }
+                }
+            )
+        )
+
+        return {
+            "type": "FeatureCollection",
+            "features": feature
+        };
+
+    }
+
     componentDidMount(){
-        const {api_url, data} = this.state;
-        fetch(api_url, { method: 'GET' })
-        .then( Response => Response.json())
-        // .then( Response => console.log("response", Response))
-        .then( Response => this.setState({
-            data: Response
-        }))
+        const {data, api_url } = this.state;
+
+        if( !data ){
+            fetch(api_url, { method: 'GET' })
+            .then( response => response.json())
+            .then( response => this.createFeatureCollection(response))
+            .then( response => this.setState({
+                data: response
+            }))
+        }
     }
 
     render (){
-        const {coords, data} = this.state;
+        const {data, map} = this.state;
+        if (data && !map) this.initializeMap();
         return (
-            <div>
-                <ReactMapGL
-                    mapboxApiAccessToken={this.state.token}
-                    {...this.state.viewport}
-                    onViewportChange={(viewport) => this.setState({viewport})}
-                >
-
-                    {data && data.map((coord, i) => (
-                        <Marker key={i} latitude={coord.point.coordinates[1]} longitude={coord.point.coordinates[0]} >
-                            <Pin />
-                        </Marker>
-                    ))}
-
-                </ReactMapGL>
-            </div>
+            <div style={{  width: 1100, height: 600, }} id="map" />
         );
     }
 
